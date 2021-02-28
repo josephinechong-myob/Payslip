e40f113189cbadcd8fa062afdf0252a559a8ceaf
Josephine Chong <josephine.chong@myob.com>
2021-02-28 22:37:26 +1100

    Payslip - Cleaning the code using string interpolation

--- pay/Program.cs
+++ pay/Program.cs
@@ -6,32 +6,14 @@ namespace pay
     {
         static void Main(string[] args)
         {
-            string welcomeMessage = "Welcome to the payslip generator";
-            string getName = "Please input your name";
-            string getSurname = "Please input your surname";
-            string getAnnualSalary = "Please enter your annual salary";
-            string getSuperRate = "Please enter your super rate";
-            string getStartDate = "Please enter your payment start date";
-            string getEndDate = "Please enter your payment end date";
-            string name;
-            string surname;
-            double salary;
-            double super;
-            string startDate;
-            string endDate;
-            double grossIncome;
-            double netIncome;
+            Console.WriteLine("Welcome to the payslip generator\nPlease input your name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Please input your surname");
+            string surname = Console.ReadLine();
+            Console.WriteLine("Please enter your annual salary");
+            double salary = Double.Parse(Console.ReadLine());
+            double grossIncome = Convert.ToInt32(Math.Floor(salary / 12));
             double incomeTax;
-            double calculatedSuper;
-
-            Console.WriteLine(welcomeMessage);
-            Console.WriteLine(getName);
-            name = Console.ReadLine();
-            Console.WriteLine(getSurname);
-            surname = Console.ReadLine();
-            Console.WriteLine(getAnnualSalary);
-            salary = Double.Parse(Console.ReadLine());
-
             if (salary > 180000)
             {
                 incomeTax = Convert.ToInt32(Math.Ceiling((54232 + (salary - 180000) * 0.45) / 12));
@@ -52,26 +34,15 @@ namespace pay
             {
                 incomeTax = 0;
             }
-
-            Console.WriteLine(getSuperRate);
-            super = Double.Parse(Console.ReadLine());
-            Console.WriteLine(getStartDate);
-            startDate = Console.ReadLine();
-            Console.WriteLine(getEndDate);
-            endDate = Console.ReadLine();
-
-            grossIncome = Convert.ToInt32(Math.Floor(salary / 12));
-            calculatedSuper = Convert.ToInt32(Math.Floor(grossIncome * (super / 100)));
-            netIncome = grossIncome - incomeTax;
-            
-            Console.WriteLine("Your payslip has been generated:");
-            Console.WriteLine("Name: " + name + " " + surname);
-            Console.WriteLine("Pay Period: " + startDate + " - " + endDate);
-            Console.WriteLine("Gross Income: " + grossIncome);
-            Console.WriteLine("Income Tax: " + incomeTax);
-            Console.WriteLine("Net Income: " + netIncome);
-            Console.WriteLine("Super: " + calculatedSuper);
-            Console.WriteLine("Thank you for using MYOB!");
+            double netIncome = grossIncome - incomeTax;
+            Console.WriteLine("Please enter your super rate");
+            double super = Double.Parse(Console.ReadLine());
+            double calculatedSuper = Convert.ToInt32(Math.Floor(grossIncome * (super / 100)));
+            Console.WriteLine("Please enter your payment start date");
+            string startDate = Console.ReadLine();
+            Console.WriteLine("Please enter your payment end date");
+            string endDate = Console.ReadLine();
+            Console.WriteLine($"Your payslip has been generated:\nName: {name} {surname}\nPay Period: {startDate} - {endDate}\nGross Income: {grossIncome}\nIncome Tax: {incomeTax}\nNet Income: {netIncome}\nSuper: {calculatedSuper}\nThank you for using MYOB!");
         }
     }
 }
